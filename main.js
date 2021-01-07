1741da85a8a566359e06f1c1714d6911026c8cc3
Barbara Kałuża <basianiemczyk1212@gmail.com>
2021-01-07 13:35:26 +0100

    add styles to navigation and background

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-const fisrtName = 'Barbara';
-const lastName = 'Anna';
-const age = 22;
+const emptyParagraph = document.querySelector('.about-me__description--js');
+emptyParagraph.innerHTML = `Potrafie zmienić tekst za pomocą JS`;
 
-console.log(`Nazywam się ${fisrtName} ${lastName} i mam ${age} lata.`);
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed`)
+    },
+    name: "Death Star",
+    age: 14979970900,
+    isOperating: true,
+
+}
+
+function hello(userName, userSurname) {
+    console.log(`Hello ${userName} ${userSurname}`) 
+}
+hello("Basia", "Kałuża");
+
+const Kasi = {
+    type: "cat",
+    eyesColor: "yellow",
+    color: "syberian grey",
+    breeding: "Niziołki Północy",
+    breed: "Syberian Cat",
+    attack: (target) => {
+        console.log(`biting ${target}`)
+    },
+    age: 3,
+    isGonna: true,
+}
+
+console.log(Kasi.attack());
